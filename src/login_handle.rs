852934cd84469578a807597038f3ac5a9ca62887
Kyler <59854022+KylerOlsen@users.noreply.github.com>
2024-06-02 23:10:07 -0600

    Added mojang authentication

--- src/login_handle.rs
+++ src/login_handle.rs
@@ -13,6 +13,8 @@ use purple_cello_mc_protocol::{
     login,
 };
 
+use purple_cello_mojang_api::multiplayer_auth;
+
 use crate::listener;
 
 const EXPIRATION_DURATION: Duration = Duration::from_secs(3600);
@@ -162,23 +164,30 @@ purplecelloserver@gmail.com".to_string())
     }
 }
 
-async fn check_player_online(
+async fn check_player(
     proxy_info: &listener::ProxyInfo,
     player: Player,
     client_conn: &mut ProtocolConnection<'_>,
 ) -> Result<PlayerAllowed> {
-    let encryption_request = client_conn.create_encryption_request(
-        proxy_info.private_key.clone())?;
-    encryption_request.write(client_conn).await?;
-    let encryption_response =
-        login::serverbound::EncryptionResponse::read(client_conn).await?;
-    client_conn.handle_encryption_response(encryption_response)?;
-    // TODO: Make authentication verification request
-    Ok(check_player_whitelist(player))
-}
-
-fn check_player_offline(player: Player) -> Result<PlayerAllowed> {
-    Ok(check_player_whitelist(player))
+    match proxy_info.online_status {
+        listener::OnlineStatus::Online => {
+            let encryption_request = client_conn.create_encryption_request(
+                proxy_info.private_key.clone())?;
+            encryption_request.write(client_conn).await?;
+            let encryption_response =
+                login::serverbound::EncryptionResponse::read(client_conn).await?;
+            client_conn.handle_encryption_response(encryption_response)?;
+            let server_id = client_conn.server_id_hash().await?;
+            match multiplayer_auth::joined(&player.name, &server_id, None).await {
+                Ok(_) => Ok(check_player_whitelist(player)),
+                Err(_) =>
+                    Ok(PlayerAllowed::False(
+                        "Mojang Authentication Failed".to_string())),
+            }
+        },
+        listener::OnlineStatus::Offline =>
+            Ok(check_player_whitelist(player)),
+    }
 }
 
 pub async fn respond_login(
@@ -222,12 +231,7 @@ async fn login_to_proxy(
         player_uuid: start_packet.player_uuid,
     };
 
-    match proxy_info.online_status {
-        listener::OnlineStatus::Online =>
-            check_player_online(proxy_info, player, client_conn).await,
-        listener::OnlineStatus::Offline =>
-            check_player_offline(player),
-    }
+    check_player(proxy_info, player, client_conn).await
 }
 
 async fn login_to_backend(
